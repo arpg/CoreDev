e87c0ac50a193bd2562ecef595b8edc3cf15d796
Jack Morrison <jackmorrison1@gmail.com>
2014-02-27 15:09:49 -0500

    Deletes update-submodules.sh

--- update-submodules.sh
+++
@@ -1,6 +0,0 @@
-cd Sophus; git pull origin master; cd ..
-cd SceneGraph; git pull origin master; cd ..
-cd Pangolin; git pull origin master; cd ..
-cd Calibu; git pull origin master; cd ..
-cd HAL; git pull origin master; cd ..
-cd GLConsole; git pull origin master; cd ..
